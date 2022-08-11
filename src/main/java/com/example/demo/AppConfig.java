99e91bb2241fce0e1382f5dfe9c5b3b94c74735c
Marcin M <mircin27@gmail.com>
2022-08-11 17:49:00 +0200

    Added annotation scope

--- src/main/java/com/example/demo/AppConfig.java
+++ src/main/java/com/example/demo/AppConfig.java
@@ -4,13 +4,16 @@ import com.example.demo.repository.HibernateSpeakerRepositoryImpl;
 import com.example.demo.repository.SpeakerRepository;
 import com.example.demo.service.SpeakerService;
 import com.example.demo.service.SpeakerServiceImpl;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean(name = "speakerService")
+    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
     public SpeakerService getSpeakerService(){
         SpeakerServiceImpl service = new SpeakerServiceImpl();
         service.setRepository(getSpeakerRepository());
